ebea45739bbfd2e781125130b60c237befef0c9c
Cell1344 <longchunlong@gmail.com>
2025-01-13 20:05:00 +0800

    cache update

--- docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
+++ docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
@@ -1,4 +1,10 @@
 import {
+  del,
+  isVue2,
+  isVue3,
+  set
+} from "./chunk-O3YQPVDG.js";
+import {
   Fragment,
   TransitionGroup,
   computed,
@@ -35,26 +41,7 @@ import {
   watch,
   watchEffect
 } from "./chunk-L2JNJ22P.js";
-
-// node_modules/vitepress/lib/vue-demi.mjs
-var isVue2 = false;
-var isVue3 = true;
-function set(target, key, val) {
-  if (Array.isArray(target)) {
-    target.length = Math.max(target.length, key);
-    target.splice(key, 1, val);
-    return val;
-  }
-  target[key] = val;
-  return val;
-}
-function del(target, key) {
-  if (Array.isArray(target)) {
-    target.splice(key, 1);
-    return;
-  }
-  delete target[key];
-}
+import "./chunk-EAWROPGL.js";
 
 // node_modules/@vueuse/shared/index.mjs
 function computedEager(fn, options) {
@@ -9332,13 +9319,4 @@ export {
   watchWithFilter,
   whenever
 };
-/*! Bundled license information:
-
-vitepress/lib/vue-demi.mjs:
-  (**
-   * vue-demi v0.14.7
-   * Copyright (c) 2020-present, Anthony Fu
-   * @license MIT
-   *)
-*/
 //# sourceMappingURL=vitepress___@vueuse_core.js.map
