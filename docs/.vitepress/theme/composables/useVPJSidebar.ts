91083cf097ec9498be57a322bf293addf5ed8fe1
Cell1344 <longchunlong@gmail.com>
2025-01-21 20:23:46 +0800

    添加了对侧边栏导航的配置处理逻辑

--- docs/.vitepress/theme/composables/useVPJSidebar.ts
+++ docs/.vitepress/theme/composables/useVPJSidebar.ts
@@ -1,16 +1,47 @@
-import { useData } from 'vitepress';
+import { useData, useRoute } from 'vitepress';
 import { computed, Ref, ref, watch } from 'vue';
 import { defineStore } from 'pinia';
 
-import type { SidebarConfig } from '../type';
+import type { SidebarConfig, SidebarNavItemData } from '../type';
 
 // @ts-ignore
 import defaultAvatar from '../assets/avatar.svg';
 
 
+function normalizePath(id: string): string {
+    /*
+    @vpj-error-note(not solved)
+    If you try to import normalizePath from vite, an error will be raised:Uncaught SyntaxError: Identifier '__vite__injectQuery' has already been declared (at ${mod.id}:40607:1)
+    */
+    return id.replace(/\\/g, '/');
+}
+
+function getNavHighlightFunc(
+    input: boolean |
+        (
+            (currentPath: string, navLinkList: string[]) => string | undefined
+        )
+) {
+    if (typeof input === 'function') return input;
+
+    return (currentPath: string, navLinkList: string[]) => {
+        const normalizeCurrent = normalizePath(currentPath);
+        const parentLinkList = navLinkList
+            .map(normalizePath)
+            .filter((path) => {
+                return normalizeCurrent.startsWith(path)
+            })
+            .sort((a: string, b: string): number => b.length - a.length);
+        return parentLinkList.length > 0 ? parentLinkList[0] : undefined;
+    }
+}
+
+
 export const useVPJSidebar = defineStore('vpj-sidebar', () => {
     // Initialize sidebar config
     const { theme, site } = useData();
+    const route = useRoute();
+
     const sidebar: Ref<SidebarConfig> = ref(theme.value.sidebar || {});
     watch(theme, (next, prev) => {
         if (JSON.stringify(next) !== JSON.stringify(prev)) {
@@ -20,6 +51,8 @@ export const useVPJSidebar = defineStore('vpj-sidebar', () => {
     const enabled = computed(() => {
         return sidebar.value.enable === undefined ? true : sidebar.value.enable
     });
+
+    // Header config
     const headerConfig = computed(() => {
         return {
             logo: sidebar.value.headerLogo || theme.value.logo || defaultAvatar,
@@ -33,6 +66,53 @@ export const useVPJSidebar = defineStore('vpj-sidebar', () => {
             title: sidebar.value.profileTitle || headerConfig.value.title,
             description: sidebar.value.profileDescription || site.value.description || ''
         }
+    })
+    // Nav config
+    const navConfig = computed(() => {
+        return {
+            links: sidebar.value.navLinks?.filter((item: SidebarNavItemData) => {
+                return item.link !== undefined &&
+                    item.text !== undefined &&
+                    item.icon !== undefined;
+            }).map((raw: SidebarNavItemData) => {
+                const item = {...raw};
+                // initialize tooltip
+                if (item.tooltip === undefined) {
+                    item.tooltip = item.text;
+                }
+                // initialize highlight
+                if (item.highlight === undefined) {
+                    item.highlight = {};
+                } else if (item.highlight === true) {
+                    item.highlight = {};
+                } else if (typeof item.highlight === 'string') {
+                    item.highlight = {
+                        normal: item.highlight,
+                        hover: item.highlight,
+                        active: item.highlight
+                    };
+                }
+                return item;
+            }) || [],
+            content: sidebar.value.navContent,
+            tooltip: sidebar.value.enableTooltip === undefined ? true : sidebar.value.enableTooltip,
+        }
+    })
+    const highlightConfig = sidebar.value.enableHighlight === undefined ? true : sidebar.value.enableHighlight;
+    let highlightFunc: (currentPath: string, navLinkList: string[]) => string | undefined;
+    if (typeof highlightConfig === 'function') {
+        highlightFunc = highlightConfig;
+    } else if (highlightConfig === true) {
+        highlightFunc = getNavHighlightFunc(true);
+    } else {
+        highlightFunc = () => undefined;
+    }
+    const highlightPath = computed(() => {
+        if (route.data.isNotFound) return undefined;
+        return highlightFunc(
+            route.path,
+            navConfig.value.links.map((item: SidebarNavItemData): string => item.link)
+        );
     })
 
     // state
@@ -51,6 +131,8 @@ export const useVPJSidebar = defineStore('vpj-sidebar', () => {
         enabled,
         headerConfig,
         profileConfig,
+        navConfig,
+        highlightPath,
         collapsed,
         toggle,
         close,
