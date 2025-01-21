67a7d4c0096277cf6ad580ad5c4bd9497f2c57a8
Cell1344 <longchunlong@gmail.com>
2025-01-21 20:25:38 +0800

    完成了侧边栏导航的内容编写

--- docs/.vitepress/theme/components/VPJSidebarNav.vue
+++ docs/.vitepress/theme/components/VPJSidebarNav.vue
@@ -1,15 +1,85 @@
 <script setup>
-import { useData } from 'vitepress';
+import { computed, ref, useTemplateRef } from 'vue';
+import { storeToRefs } from 'pinia';
 
-const { site, frontmatter, theme } = useData();
+import { useVPJSidebar } from '../composables/useVPJSidebar';
+import VPJSidebarNavLink from './VPJSidebarNavLink.vue';
+
+
+const store = useVPJSidebar();
+const { collapsed, navConfig } = storeToRefs(store);
 </script>
 
 
 <template>
-Nav
+    <nav :class="['vpj-sidebar-nav', 'vpj-scroll-y', {'collapsed': collapsed}]">
+        <ul class="vpj-sidebar-nav__link-list">
+            <li 
+                v-for="item in navConfig.links"
+                :key="item.text"
+                class="vpj-sidebar-nav__link-item"
+            >
+                <VPJSidebarNavLink
+                    :icon="item.icon"
+                    :text="item.text"
+                    :link="item.link"
+                    :tooltip="item.tooltip"
+                    :highlight="item.highlight"
+                    class="vpj-sidebar-nav__link"
+                />
+            </li>
+        </ul>
+        <component v-if="navConfig.content" :is="navConfig.content"/>
+    </nav>
 </template>
 
 
 <style scoped>
+    /* Nav layout */
+    .vpj-sidebar-nav {
+        align-items: center;
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        gap: .625rem;
+        min-height: 0;
+        overflow-x: hidden;
+        padding-bottom: .75rem;
+        padding-left: .75rem;
+        padding-right: calc(.75rem - 4px);
+        width: 100%;
+    }
 
+    /* Nav links */
+    .vpj-sidebar-nav__link-list {
+        display: flex;
+        flex-direction: column;
+        flex-shrink: 0;
+        gap: .125rem;
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        position: relative;
+        width: 100%;
+    }
+
+    .vpj-sidebar-nav__link-item {
+        position: relative;
+    }
+
+    /* StyleSheet for collapsed state */
+    .vpj-sidebar-nav.collapsed {
+        padding-left: 12px;
+        padding-right: 8px;
+    }
+
+    .vpj-sidebar-nav.collapsed .vpj-sidebar-nav__link-list {
+        gap: .625rem;
+    }
+
+    /* Nav links */
+    .vpj-sidebar-nav.collapsed .vpj-sidebar-nav__link {
+        width: 36px;
+        padding: 10px;
+    }
 </style>
