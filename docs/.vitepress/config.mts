552e4a4c305121f29ac248d5b87fd476509831a8
Cell1344 <longchunlong@gmail.com>
2025-01-21 19:19:22 +0800

    完善了侧边栏导航的配置类型与配置内容

--- docs/.vitepress/config.mts
+++ docs/.vitepress/config.mts
@@ -8,7 +8,12 @@ export default defineConfigWithTheme<ThemeConfig>({
     description: "Cell's personal site",
     themeConfig: {
         sidebar: {
-            profileDescription: "这里是Cell的个人站点，记录了一些日常的学习心得，仍在建设中。"
+            profileDescription: "这里是Cell的个人站点，记录了一些日常的学习心得，仍在建设中。",
+            navLinks: [
+                {icon: "VPJIconHome", text: "我的主页", link: "/", tooltip: "主页"},
+                {icon: "VPJIconBlogPencil", text: "我的博客", link: "/blogs/", tooltip: "博客"},
+                {icon: "VPJIconBookBookmark", text: "我的笔记", link: "/docs/", tooltip: "笔记"}
+            ],
         }
     }
 })
--- docs/.vitepress/theme/type.ts
+++ docs/.vitepress/theme/type.ts
@@ -1,9 +1,44 @@
 export type ImageData = string | { src: string; alt?: string };
 
-export type NavItemData = {
-    icon: ImageData | { component: string };
+export type SidebarNavItemData = {
+    /**
+     * 侧边栏导航项的图标，可以是一个路径字符串或者一个组件，也可以通过对象指定图片的更多属性
+     * @requires
+     */
+    icon:
+        ImageData |
+        { component: string };
+
+    /**
+     * 侧边栏导航项的文本，接受一个字符串
+     * @requires
+     */
     text: string;
+
+    /**
+     * 侧边栏导航项的链接，接受一个路径字符串
+     * @requires
+     */
     link: string;
+
+    /**
+     * 侧边栏导航项的提示文本，接受一个字符串作为弹出的提示文本，也可以设置为null来单独禁用某一项的提示文本
+     * @optional
+     * @defaultValue item.text 默认使用该项的文本作为提示文本
+     */
+    tooltip?:
+        string |
+        null;
+
+    /**
+     * 侧边栏导航项是否可以高亮，可以接受一个boolean值表示是否启用，也可以接受一个字符串或一个对象来指定高亮下图标的颜色（仅对使用svg图标的情况生效，对使用图片路径指定图标的项此设置无效）
+     * @optional
+     * @defaultValue true 默认启用高亮
+     */
+    highlight?:
+        boolean |
+        string |
+        {normal?: string; hover?: string; active?: string};
 };
 
 export interface SidebarConfig {
@@ -65,9 +100,25 @@ export interface SidebarConfig {
     /**
      * 自定义侧边栏的导航项列表
      * @optional
-     * @defaultValue [] 默认情况下没有导航
+     * @defaultValue [] 默认情况下没有导航项
      */
-    navLinks?: NavItemData[];
+    navLinks?: SidebarNavItemData[];
+
+    /**
+     * 是否启用侧边栏导航项的提示文本，这会在侧边栏收起时为鼠标悬浮时显示一个提示，当设置为false时，将禁用所有导航项单独的tooltip配置
+     * @optional
+     * @defaultValue true 默认为启用提示文本，可以在navLinks中单独禁用或修改某项的tooltip配置
+     */
+    enableTooltip?: boolean;
+
+    /**
+     * 是否高亮当前页面最接近的父路径，这会为当前路由匹配到的最近父路径项的图标添加一个高亮样式。也可以输入一个接受函数来自定义高亮项的逻辑
+     * @optional
+     * @defaultValue true 默认为true
+     */
+    enableHighlight?:
+        boolean |
+        ((currentPath: string, navLinkList: string[]) => string | undefined);
 
     /**
      * 自定义侧边栏的额外内容，接受一个已经在全局注册的组件名，这将会被放置在导航栏下方的位置，并且仅在侧边栏展开时显示。
