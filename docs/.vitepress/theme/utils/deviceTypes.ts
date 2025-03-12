2110a943650ddbced1824a54fdd43c7333d40bbf
Cell1344 <longchunlong@gmail.com>
2025-03-12 21:04:53 +0800

    修改了用于判断设备类型变量所使用的css media查询表达式，使得它们与之前的代码有更好的一致性

--- docs/.vitepress/theme/utils/deviceTypes.ts
+++ docs/.vitepress/theme/utils/deviceTypes.ts
@@ -2,10 +2,10 @@ import { useMediaQuery } from '@vueuse/core';
 import { computed } from 'vue';
 
 // @ts-ignore
-export const larger1024 = useMediaQuery('(min-width: 1024px)', { ssrWidth: 1200 });
+export const smaller1024 = useMediaQuery('(max-width: 1024px)', { ssrWidth: 1200 });
 // @ts-ignore
-export const larger768 = useMediaQuery('(min-width: 768px)', { ssrWidth: 1200 });
+export const smaller768 = useMediaQuery('(max-width: 768px)', { ssrWidth: 1200 });
 
-export const isMobile = computed(() => !larger768.value);
-export const isTablet = computed(() => larger768.value && !larger1024.value);
-export const isDesktop = computed(() => larger1024.value);
+export const isMobile = computed<boolean>(() => smaller768.value);
+export const isTablet = computed<boolean>(() => !smaller768.value && smaller1024.value);
+export const isDesktop = computed<boolean>(() => !smaller1024.value);
