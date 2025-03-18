15f5771c77edcbfd972a29891ab10e7452f49426
Cell1344 <longchunlong@gmail.com>
2025-03-18 20:19:30 +0800

    添加了用于blog布局与doc布局的横幅图片组件

---
+++ docs/.vitepress/theme/components/VPJCoverImage.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="vpj-layout-blog__cover-image">
+
+    </div>
+</template>
+
+
+<style scoped>
+    .vpj-layout-blog__cover-image {
+        background-color: lightcoral;
+        flex-shrink: 0;
+        height: 240px;
+        width: 100%;
+    }
+</style>
