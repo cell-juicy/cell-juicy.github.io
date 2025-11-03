7908ef692d286b8bce01f39984bfcc781bc6c34d
Cell1344 <longchunlong@gmail.com>
2025-11-03 15:47:07 +0800

    添加了评论的配置类型

---
+++ docs/.vitepress/theme/types/comment.ts
@@ -0,0 +1,31 @@
+import type { Route } from "vitepress";
+
+
+export interface GiscusOptions {
+    repo: string;
+    repoId: string;
+    category: string;
+    categoryId: string;
+    strict?: boolean;
+    reactionsEnabled?: boolean;
+    emitMetadata?: boolean;
+    inputPosition?:
+        | "bottom"
+        | "top";
+    term?:
+        | string
+        | ((route: Route) => string)
+    theme?:
+        | string
+        | {
+            light?: string;
+            dark?: string;
+        };
+    lang?: string;
+    lazyLoading?: boolean;
+}
+
+export type CommentProviderConfig = {
+    provider: "giscus",
+    option: GiscusOptions,
+}
