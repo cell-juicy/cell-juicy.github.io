15ab5559e40217f1d48771e100035dd5acd2631f
Cell1344 <longchunlong@gmail.com>
2025-11-04 21:11:16 +0800

    为blog/doc布局添加了评论按钮与相关配置接口

--- docs/.vitepress/theme/composables/useVPJLayout.ts
+++ docs/.vitepress/theme/composables/useVPJLayout.ts
@@ -79,6 +79,10 @@ const DEFAULT = {
             enabled: true,
             tooltip: "查看历史记录"
         },
+        COMMENT: {
+            enabled: true,
+            tooltip: "评论本页"
+        },
         ASIDETABS: {
             series: {name: "系列", component: "VPJArticleAsideSeriesPage", order: 0},
             tags: {name: "标签", component: "VPJArticleAsideTagsPage", order: 0},
@@ -126,6 +130,10 @@ const DEFAULT = {
         HISTORY: {
             enabled: true,
             tooltip: "查看历史记录"
+        },
+        COMMENT: {
+            enabled: true,
+            tooltip: "评论本页"
         },
         ASIDETABS: {
             tree: {name: "目录", component: "VPJArticleAsideTreePage", order: 0},
@@ -435,6 +443,12 @@ export const useVPJLayout = defineStore("vpj-layout", () => {
                 (layoutConfig.value as VPJBlogLayoutConfig|VPJDocLayoutConfig).history,
                 defaultConfig.value.HISTORY
             );
+            const comment = toolbarFeatureMerger(
+                frontmatter.value.comment,
+                specificConfig.value.comment,
+                (layoutConfig.value as VPJBlogLayoutConfig|VPJDocLayoutConfig).comment,
+                defaultConfig.value.COMMENT
+            );
 
             return {
                 headerTitle,
@@ -443,7 +457,8 @@ export const useVPJLayout = defineStore("vpj-layout", () => {
                 pdf,
                 md,
                 toolbar,
-                history
+                history,
+                comment
             }
         };
         return undefined;
